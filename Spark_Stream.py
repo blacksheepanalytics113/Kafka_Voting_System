14cca6754b1e3531bd3ff48ebdc841d52f61cb88
Adun Ajiye <adunajiye@gmail.com>
2023-12-21 18:08:41 +0100

    Spark

--- Spark_Stream.py
+++ Spark_Stream.py
@@ -17,13 +17,43 @@ print(pyspark.__version__)
     # Initialize SparkSession
 spark = (SparkSession.builder
             .appName("ElectionAnalysis")
-             .master("local[*]")  # Use local Spark execution with all available cores
+             .master("local[*]")  
              .config("spark.jars.packages",
-                     "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0")  # Spark-Kafka integration
+                     "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0")  
              .config("spark.jars",
-                     "C:/Users/user/Desktop/Kafka_Voting_System/jar/postgresql-42.7.1")  # PostgreSQL driver
-             .config("spark.sql.adaptive.enabled", "false")  # Disable adaptive query execution
+                     "C:/Users/user/Desktop/Kafka_Voting_System/jar/postgresql-42.7.1")  
+             .config("spark.sql.adaptive.enabled", "false") 
              .getOrCreate())
-print(spark)
+# print(spark)
+# Define schemas for Kafka topics
+vote_schema = StructType([
+        StructField("voter_id", StringType(), True),
+        StructField("candidate_id", StringType(), True),
+        StructField("voting_time", TimestampType(), True),
+        StructField("voter_name", StringType(), True),
+        StructField("party_affiliation", StringType(), True),
+        StructField("biography", StringType(), True),
+        StructField("campaign_platform", StringType(), True),
+        StructField("photo_url", StringType(), True),
+        StructField("candidate_name", StringType(), True),
+        StructField("date_of_birth", StringType(), True),
+        StructField("gender", StringType(), True),
+        StructField("nationality", StringType(), True),
+        StructField("registration_number", StringType(), True),
+        StructField("address", StructType([
+            StructField("street", StringType(), True),
+            StructField("city", StringType(), True),
+            StructField("state", StringType(), True),
+            StructField("country", StringType(), True),
+            StructField("postcode", StringType(), True)
+        ]), True),
+        StructField("email", StringType(), True),
+        StructField("phone_number", StringType(), True),
+        StructField("cell_number", StringType(), True),
+        StructField("picture", StringType(), True),
+        StructField("registered_age", IntegerType(), True),
+        StructField("vote", IntegerType(), True)
+    ])
+print(vote_schema)
 
              
